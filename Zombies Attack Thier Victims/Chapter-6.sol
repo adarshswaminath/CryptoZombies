ec866e93820f9aacc0f9e7a87d493ed2441219da
adarshswaminath <adarshswaminath7@gmail.com>
2023-02-02 23:24:27 +0530

    Zombies Attack Thier Victims

---
+++ Zombies Attack Thier Victims/Chapter-6.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+// put import statement here
+import "./zombiefactory.sol";
+
+contract ZombieFeeding is ZombieFactory {
+
+}
---
+++ Zombies Attack Thier Victims/Chapter-7.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "./zombiefactory.sol";
+
+contract ZombieFeeding is ZombieFactory {
+
+  // Start here
+  function feedAndMultiply(uint _zombieId,uint _targetDna) public {
+      require(msg.sender == zombieToOwner[_zombieId]);
+      Zombie storage myZombie = zombies[_zombieId];
+
+  }
+
+}
